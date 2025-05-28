7c34613dea62a81bc15897483669d7160686a87f
Asant525 <bigjoedigitals2004@gmail.com>
2025-05-28 07:30:52 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
-const settings = {
+Aconst settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'knight Bot', // Your name
+  ownerNumber: 233591010944', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
